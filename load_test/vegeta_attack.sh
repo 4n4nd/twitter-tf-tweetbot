15c0ebeccd16db3a3436037510af17a7dbd63eb7
Anand Sanmukhani <asanmukh@redhat.com>
2019-04-25 09:50:14 -0400

    Add comments to sample vegeta attack script

--- load_test/vegeta_attack.sh
+++ load_test/vegeta_attack.sh
@@ -1,3 +1,4 @@
+# attack the specified target with 10 requests per second for 180 seconds
 vegeta attack -targets=./targets.txt -name=10qps -rate=10/s -duration=180s > results.10qps.bin
-
+# Plot the output from the attack in an html
 vegeta plot results.10qps.bin > plot.html
